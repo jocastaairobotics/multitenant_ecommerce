16c5b19aacf6058473ac7f369520d68222386c14
“Waseem <waseem@binarysoftech.co.in>
2023-11-07 12:06:35 +0530

    single object or list of object

--- Products/forms.py
+++ Products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(max_length=254)
-    description = forms.CharField(widget=forms.Textarea)
-    price = forms.IntegerField()
-    rating = forms.FloatField()
+    kanif = forms.CharField(max_length=254)
+    santoshi = forms.CharField(widget=forms.Textarea)
+    niraj = forms.IntegerField()
+    pratik = forms.FloatField()
--- Products/views.py
+++ Products/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ProductForm
+from .models import Product
 
 
 def productHome(request):
     form = ProductForm()
+    p = Product.objects.all()
     ctx = {
-        "form": form
+        "form": form,
+        "product": p
     }
     return render(request, template_name='products/index.html', context=ctx)
 
 
 def createProduct(request):
     data = request.POST
-    name = data.get("name")
-    description = data.get("description")
-    price = data.get("price")
-    rating = data.get("rating")
-    print(data)
+    name = data.get("kanif")
+    description = data.get("santoshi")
+    price = data.get("niraj")
+    rating = data.get("pratik")
+
+    p = Product(name=name, description=description, price=price, rating=rating)
+    p.save()
+
     return redirect("/product/index")
